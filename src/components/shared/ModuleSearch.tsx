54226fe1e3aeaa5db18e39b6d2168eacaca468d5
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-05-25 03:36:13 +0700

    Manga

--- src/components/shared/ModuleSearch.tsx
+++ src/components/shared/ModuleSearch.tsx
@@ -4,7 +4,6 @@ const ModuleSearch: React.FC = () => {
     return (
         <div className="absolute min-w-[700px] min-h-[300px] bg-background-900 top-[70px] right-32 rounded-md p-4 after:w-[18px] after:h-[18px] after:inline-block after:absolute after:top-[-8px] after:left-80 after:rotate-45 after:bg-background-900">
             Module Search
-            
         </div>
     )
 }
