92ef856a1bc3594a3d97d0d1968940ca9a26678b
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-05-26 15:14:02 +0700

    🚒 add footer 🚒

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 export const REVALIDATE_TIME = 18000; // 5 hours
-export const WEBSITE_URL = 'https://akame.vercel.app/'
+export const WEBSITE_URL = 'https://akame.vercel.app/';
+export const DISCORD_URL = 'https://discord.gg/fjkzXy';
+export const FACEBOOK_URL = 'https://www.facebook.com/akame.vercel';
